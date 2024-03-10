6fe59d6a3ac20394360e8456b4d2a9f6d21a95de
mayapoojari <mayadevipoojari196@gmail.com>
2024-03-10 21:31:14 +0530

    Describe this commit

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main(void) {
-  cout << "Hello, PES2UG21CS284\n";
+  cout << "Hello, PES2UG21CS573\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
   return 0;
